99a0135a5cbe8f6b662fb35ac9ea3421fab7df42
bstafford2017 <b.stafford2017@gmail.com>
2025-12-08 21:21:13 -0600

    Fix upload

--- src/handlers/ask.py
+++ src/handlers/ask.py
@@ -3,7 +3,9 @@ import sys
 import os
 import logging
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+# Add paths for Lambda environment
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, '/var/task')
 
 from agents import supervisor
 
